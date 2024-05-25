c5c0141cedf1c6bef943e42627c50f5e825a1edf
Wintec-Yuda <alamasta004@gmail.com>
2024-05-25 20:40:36 +0700

    setup nestjs & CRUD Article restful API & setup prisma database

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { Article } from './entities/article.entity';
+
+@Controller('articles')
+@ApiTags('articles')
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Post()
+  @ApiCreatedResponse({ type: Article })
+  create(@Body() createArticleDto: CreateArticleDto) {
+    return this.articlesService.create(createArticleDto);
+  }
+
+  @Get('drafts')
+  @ApiCreatedResponse({ type: Article, isArray: true })
+  findDrafts() {
+    return this.articlesService.findDrafts();
+  }
+
+  @Get()
+  @ApiCreatedResponse({ type: Article, isArray: true })
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  @ApiCreatedResponse({ type: Article })
+  findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiCreatedResponse({ type: Article })
+  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(+id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  @ApiCreatedResponse({ type: Article })
+  remove(@Param('id') id: string) {
+    return this.articlesService.remove(+id);
+  }
+}
